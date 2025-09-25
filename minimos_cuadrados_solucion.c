d445771fa43257dbb66fac015f0406fb0c7b8b72
LTprograms <105554066+LTprograms@users.noreply.github.com>
2025-09-24 20:37:01 -0500

    .

--- minimos_cuadrados_solucion.c
+++ minimos_cuadrados_solucion.c
@@ -1,186 +1,281 @@
-/* ------------------------------------------------------------------------
- * ARCHIVO: minimos_cuadrados.c
- *
- * DESCRIPCION DEL PROBLEMA:
- *  El metodo de minimos cuadrados es una tecnica estandar utilizada para encontrar
- *  la ecuacion de una linea recta a partir de un conjunto de datos. La ecuacion para una
- *  linea recta esta dada por
- *	 y = mx + b
- *  donde m es la pendiente de la linea y b es la interseccion con el eje y.
- *
- *  Dado un conjunto de n puntos {(x1,y1), x2,y2),...,xn,yn)}, sea
- *      SUMAx = x1 + x2 + ... + xn
- *      SUMAy = y1 + y2 + ... + yn
- *      SUMAxy = x1*y1 + x2*y2 + ... + xn*yn
- *      SUMAxx = x1*x1 + x2*x2 + ... + xn*xn
- *
- *  La pendiente e interseccion con y para la linea de minimos cuadrados puede entonces ser
- *  calculada usando las siguientes ecuaciones:
- *        pendiente (m) = ( SUMAx*SUMAy - n*SUMAxy ) / ( SUMAx*SUMAx - n*SUMAxx )
- *  interseccion-y (b) = ( SUMAy - pendiente*SUMAx ) / n
- *
- * DESCRIPCION DEL PROGRAMA:
- *  o Este programa calcula un modelo lineal para un conjunto de datos dados.
- *  o Los datos se leen desde un archivo; la primera linea es el numero de puntos
- *    de datos (n), seguido por las coordenadas de x e y.
- *  o Los puntos de datos se dividen entre procesos de tal manera que cada proceso
- *    tiene promedio_n = n/numero_procesos puntos; los puntos de datos restantes se
- *    agregan al ultimo proceso.
- *  o Cada proceso calcula las sumas parciales (miSUMAx,miSUMAy,miSUMAxy,
- *    miSUMAxx) independientemente, usando su subconjunto de datos. En el paso final,
- *    las sumas globales (SUMAx,SUMAy,SUMAxy,SUMAxx) se calculan para encontrar
- *    la linea de minimos cuadrados.
- *  o Para el proposito de este ejercicio, la comunicacion se hace estrictamente
- *    usando las operaciones punto a punto de MPI, MPI_SEND y MPI_RECV.
- *
- * USO: Probado para ejecutar usando 1,2,...,10 procesos.
- *
- * AUTOR: Dora Abdullah (version MPI, 11/96)
- * ULTIMA REVISION: RYL convertido a C (12/11)
- * ---------------------------------------------------------------------- */
+#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <stdio.h>
 #include "mpi.h"
 
 int main(int argc, char **argv) {
+    int mi_id, numero_procesos;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &mi_id);
+    MPI_Comm_size(MPI_COMM_WORLD, &numero_procesos);
 
-  double *x, *y;
-  double miSUMAx, miSUMAy, miSUMAxy, miSUMAxx, SUMAx, SUMAy, SUMAxy,
-         SUMAxx, SUMAresidual, residual, pendiente, interseccion_y, y_estimado;
-  int i,j,n,mi_id,numero_procesos,promedio_n,n_restante,mis_puntos,desplazamiento_i;
-  /*int pausa_personalizada (int segundos);*/
-  MPI_Status estado_i;
-  FILE *archivo_entrada;
+    int n = 0; /* numero de puntos total */
+    double *x_full = NULL, *y_full = NULL; /* solo usados por proceso 0 */
+    int promedio_n, n_restante;
 
-  archivo_entrada = fopen("datos_xy.txt", "r");
-  if (archivo_entrada == NULL) printf("error abriendo archivo\n");
+    /* Variables locales para cada proceso */
+    int mis_puntos = 0;
+    int desplazamiento = 0; /* índice inicial en el arreglo global */
+    double *x_local = NULL, *y_local = NULL;
 
-  MPI_Init(&argc, &argv);
-  MPI_Comm_rank (MPI_COMM_WORLD, &mi_id);
-  MPI_Comm_size (MPI_COMM_WORLD, &numero_procesos);
+    MPI_Status status;
+    MPI_Request req;
 
-  /* ----------------------------------------------------------
-   * Paso 1: El proceso 0 lee datos y envia el valor de n
-   * ---------------------------------------------------------- */
-  if (mi_id == 0) {
-    printf ("Numero de procesos utilizados: %d\n", numero_procesos);
-    printf ("-------------------------------------\n");
-    printf ("Las coordenadas x en los procesos trabajadores:\n");
-    /* esta llamada se usa para lograr un formato de salida consistente */
-    /* pausa_personalizada (3);*/
-    fscanf (archivo_entrada, "%d", &n);
-    x = (double *) malloc (n*sizeof(double));
-    y = (double *) malloc (n*sizeof(double));
-    for (i=0; i<n; i++)
-      fscanf (archivo_entrada, "%lf %lf", &x[i], &y[i]);
-    for (i=1; i<numero_procesos; i++)
-      MPI_Send (&n, 1, MPI_INT, i, 10, MPI_COMM_WORLD);
-  }
-  else {
-    MPI_Recv (&n, 1, MPI_INT, 0, 10, MPI_COMM_WORLD, &estado_i);
-    x = (double *) malloc (n*sizeof(double));
-    y = (double *) malloc (n*sizeof(double));
-  }
-  /* ---------------------------------------------------------- */
-  
-  promedio_n = n/numero_procesos;
-  n_restante = n % numero_procesos;
-
-  /* ----------------------------------------------------------
-   * Paso 2: El proceso 0 envia subconjuntos de x e y
-   * ---------------------------------------------------------- */
-  if (mi_id == 0) {
-    for (i=1; i<numero_procesos; i++) {
-      desplazamiento_i = i*promedio_n;
-      mis_puntos = (i < numero_procesos -1) ? promedio_n : promedio_n + n_restante;
-      MPI_Send (&desplazamiento_i, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
-      MPI_Send (&mis_puntos, 1, MPI_INT, i, 2, MPI_COMM_WORLD);
-      MPI_Send (&x[desplazamiento_i], mis_puntos, MPI_DOUBLE, i, 3, MPI_COMM_WORLD);
-      MPI_Send (&y[desplazamiento_i], mis_puntos, MPI_DOUBLE, i, 4, MPI_COMM_WORLD);
+    /******************************
+     * Paso 0: Proceso 0 lee el archivo y distribuye n
+     ******************************/
+    if (mi_id == 0) {
+        FILE *archivo_entrada = fopen("datos_xy.txt", "r");
+        if (!archivo_entrada) {
+            fprintf(stderr, "Error abriendo archivo datos_xy.txt\n");
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        if (fscanf(archivo_entrada, "%d", &n) != 1) {
+            fprintf(stderr, "Error leyendo n desde archivo\n");
+            fclose(archivo_entrada);
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        /* leer todos los datos */
+        x_full = (double *) malloc(n * sizeof(double));
+        y_full = (double *) malloc(n * sizeof(double));
+        for (int i = 0; i < n; ++i) {
+            if (fscanf(archivo_entrada, "%lf %lf", &x_full[i], &y_full[i]) != 2) {
+                fprintf(stderr, "Error leyendo punto %d\n", i);
+                fclose(archivo_entrada);
+                MPI_Abort(MPI_COMM_WORLD, 1);
+            }
+        }
+        fclose(archivo_entrada);
     }
-  }
-  else {
-    /* ---------------los otros procesos reciben---------------- */
-    MPI_Recv (&desplazamiento_i, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &estado_i);
-    MPI_Recv (&mis_puntos, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, &estado_i);
-    MPI_Recv (&x[desplazamiento_i], mis_puntos, MPI_DOUBLE, 0, 3, MPI_COMM_WORLD,
-	      &estado_i);
-    MPI_Recv (&y[desplazamiento_i], mis_puntos, MPI_DOUBLE, 0, 4, MPI_COMM_WORLD,
-	      &estado_i);
-    printf ("id %d: ", mi_id);
-    for (i=0; i<n; i++) printf("%4.2lf ", x[i]);
-    printf ("\n");
-    /* ---------------------------------------------------------- */
-  }
 
-  /* ----------------------------------------------------------
-   * Paso 3: Cada proceso calcula su suma parcial
-   * ---------------------------------------------------------- */
-  miSUMAx = 0; miSUMAy = 0; miSUMAxy = 0; miSUMAxx = 0;
-  if (mi_id == 0) {
-    desplazamiento_i = 0;
-    mis_puntos = promedio_n;
-  }
-  for (j=0; j<mis_puntos; j++) {
-    miSUMAx = miSUMAx + x[desplazamiento_i+j];
-    miSUMAy = miSUMAy + y[desplazamiento_i+j];
-    miSUMAxy = miSUMAxy + x[desplazamiento_i+j]*y[desplazamiento_i+j];
-    miSUMAxx = miSUMAxx + x[desplazamiento_i+j]*x[desplazamiento_i+j];
-  }
-  
-  /* ----------------------------------------------------------
-   * Paso 4: El proceso 0 recibe sumas parciales de los otros
-   * ---------------------------------------------------------- */
-  if (mi_id != 0) {
-    MPI_Send (&miSUMAx, 1, MPI_DOUBLE, 0, 5, MPI_COMM_WORLD);
-    MPI_Send (&miSUMAy, 1, MPI_DOUBLE, 0, 6, MPI_COMM_WORLD);
-    MPI_Send (&miSUMAxy,1, MPI_DOUBLE, 0, 7, MPI_COMM_WORLD);
-    MPI_Send (&miSUMAxx,1, MPI_DOUBLE, 0, 8, MPI_COMM_WORLD);
-	    }
-  else {
-    SUMAx = miSUMAx; SUMAy = miSUMAy;
-    SUMAxy = miSUMAxy; SUMAxx = miSUMAxx;
-    for (i=1; i<numero_procesos; i++) {
-      MPI_Recv (&miSUMAx, 1, MPI_DOUBLE, i, 5, MPI_COMM_WORLD, &estado_i);
-      MPI_Recv (&miSUMAy, 1, MPI_DOUBLE, i, 6, MPI_COMM_WORLD, &estado_i);
-      MPI_Recv (&miSUMAxy,1, MPI_DOUBLE, i, 7, MPI_COMM_WORLD, &estado_i);
-      MPI_Recv (&miSUMAxx,1, MPI_DOUBLE, i, 8, MPI_COMM_WORLD, &estado_i);
-      SUMAx = SUMAx + miSUMAx;
-      SUMAy = SUMAy + miSUMAy;
-      SUMAxy = SUMAxy + miSUMAxy;
-      SUMAxx = SUMAxx + miSUMAxx;
+    /* Enviar el n a todos (no bloqueante). Los demás procesos hacen Irecv. */
+    if (mi_id == 0) {
+        /* proceso 0 envía n a todos (incluye a sí mismo, pero no es necesario) */
+        for (int p = 1; p < numero_procesos; ++p) {
+            MPI_Isend(&n, 1, MPI_INT, p, 100, MPI_COMM_WORLD, &req);
+            MPI_Wait(&req, &status);
+        }
+    } else {
+        MPI_Irecv(&n, 1, MPI_INT, 0, 100, MPI_COMM_WORLD, &req);
+        MPI_Wait(&req, &status);
     }
-  }
 
-  /* ----------------------------------------------------------
-   * Paso 5: El proceso 0 hace los pasos finales
-   * ---------------------------------------------------------- */
-  if (mi_id == 0) {
-    pendiente = ( SUMAx*SUMAy - n*SUMAxy ) / ( SUMAx*SUMAx - n*SUMAxx );
-    interseccion_y = ( SUMAy - pendiente*SUMAx ) / n;
-    /* esta llamada se usa para lograr un formato de salida consistente */
-    /*pausa_personalizada (3);*/
-    printf ("\n");
-    printf ("La ecuacion lineal que mejor se ajusta a los datos dados:\n");
-    printf ("       y = %6.2lfx + %6.2lf\n", pendiente, interseccion_y);
-    printf ("--------------------------------------------------\n");
-    printf ("   Original (x,y)     Y estimado     Residual\n");
-    printf ("--------------------------------------------------\n");
-
-    SUMAresidual = 0;
-    for (i=0; i<n; i++) {
-      y_estimado = pendiente*x[i] + interseccion_y;
-      residual = y[i] - y_estimado;
-      SUMAresidual = SUMAresidual + residual*residual;
-      printf ("   (%6.2lf %6.2lf)      %6.2lf       %6.2lf\n",
-	      x[i], y[i], y_estimado, residual);
+    /* todos ahora conocen n */
+    if (n <= 0) {
+        if (mi_id == 0) fprintf(stderr, "n debe ser > 0\n");
+        MPI_Finalize();
+        return 0;
     }
-    printf("--------------------------------------------------\n");
-    printf("Suma residual = %6.2lf\n", SUMAresidual);
-  }
 
-  /* ----------------------------------------------------------	*/
-  MPI_Finalize();
-  return 0;
+    /* calcular reparto equilibrado: los primeros n_restante procesos obtienen q+1 */
+    promedio_n = n / numero_procesos;     /* q */
+    n_restante = n % numero_procesos;     /* r */
+    /* Para cada proceso p:
+     *   if (p < r) mis_puntos = q+1, desplazamiento = p*(q+1)
+     *   else mis_puntos = q, desplazamiento = r*(q+1) + (p-r)*q
+     * Esto equilibra la carga mejor que dar todo al ultimo proceso.
+     */
+
+    if (mi_id < n_restante) {
+        mis_puntos = promedio_n + 1;
+        desplazamiento = mi_id * mis_puntos;
+    } else {
+        mis_puntos = promedio_n;
+        desplazamiento = n_restante * (promedio_n + 1) + (mi_id - n_restante) * promedio_n;
+    }
+
+    /* Todos reservan espacio para su porcion local */
+    if (mis_puntos > 0) {
+        x_local = (double *) malloc(mis_puntos * sizeof(double));
+        y_local = (double *) malloc(mis_puntos * sizeof(double));
+    } else {
+        /* caso raro cuando numero_procesos > n */
+        x_local = NULL;
+        y_local = NULL;
+    }
+
+    /******************************
+     * Paso 1: Proceso 0 distribuye subconjuntos a todos (no bloqueante)
+     *
+     * Estrategia:
+     *  - Proceso 0 envía a cada proceso p su mis_puntos y luego su fragmento x,y.
+     *  - Usamos Isend/Irecv y Wait/Waitall para experimentar con comunicaciones no bloqueantes.
+     ******************************/
+    if (mi_id == 0) {
+        /* proceso 0 copia su propia porcion desde x_full/y_full */
+        if (mis_puntos > 0) {
+            for (int i = 0; i < mis_puntos; ++i) {
+                x_local[i] = x_full[desplazamiento + i];
+                y_local[i] = y_full[desplazamiento + i];
+            }
+        }
+
+        /* enviar los fragmentos a los procesos 1..P-1 de forma no bloqueante */
+        for (int p = 1; p < numero_procesos; ++p) {
+            int p_mis_puntos, p_desplazamiento;
+            if (p < n_restante) {
+                p_mis_puntos = promedio_n + 1;
+                p_desplazamiento = p * p_mis_puntos;
+            } else {
+                p_mis_puntos = promedio_n;
+                p_desplazamiento = n_restante * (promedio_n + 1) + (p - n_restante) * promedio_n;
+            }
+
+            /* Enviar primero la cantidad p_mis_puntos (int) */
+            MPI_Isend(&p_mis_puntos, 1, MPI_INT, p, 110, MPI_COMM_WORLD, &req);
+            MPI_Wait(&req, &status);
+
+            if (p_mis_puntos > 0) {
+                /* Enviar el arreglo x y y (Isend + Wait) */
+                MPI_Isend(&x_full[p_desplazamiento], p_mis_puntos, MPI_DOUBLE, p, 111, MPI_COMM_WORLD, &req);
+                MPI_Wait(&req, &status);
+                MPI_Isend(&y_full[p_desplazamiento], p_mis_puntos, MPI_DOUBLE, p, 112, MPI_COMM_WORLD, &req);
+                MPI_Wait(&req, &status);
+            }
+        }
+
+    } else {
+        /* procesos distintos de 0 reciben su p_mis_puntos y luego los arreglos */
+        MPI_Irecv(&mis_puntos, 1, MPI_INT, 0, 110, MPI_COMM_WORLD, &req);
+        MPI_Wait(&req, &status);
+
+        /* Ajustar si recibimos 0 puntos */
+        if (mis_puntos > 0) {
+            /* reasignar memoria si es distinto (por seguridad) */
+            free(x_local);
+            free(y_local);
+            x_local = (double *) malloc(mis_puntos * sizeof(double));
+            y_local = (double *) malloc(mis_puntos * sizeof(double));
+
+            MPI_Irecv(x_local, mis_puntos, MPI_DOUBLE, 0, 111, MPI_COMM_WORLD, &req);
+            MPI_Wait(&req, &status);
+            MPI_Irecv(y_local, mis_puntos, MPI_DOUBLE, 0, 112, MPI_COMM_WORLD, &req);
+            MPI_Wait(&req, &status);
+        }
+    }
+
+    /* En este punto, cada proceso tiene su subconjunto local en x_local,y_local
+     * y la variable mis_puntos contiene cuantos puntos tiene cada proceso.
+     */
+
+    /* Imprimir (opcional) los datos locales para verificar el reparto (descomentar si se desea) */
+    /*
+    printf("Proc %d: mis_puntos=%d desplazamiento=%d\n", mi_id, mis_puntos, desplazamiento);
+    for (int ii=0; ii<mis_puntos; ++ii) {
+        printf("  proc %d: x[%d]=%f y=%f\n", mi_id, ii, x_local[ii], y_local[ii]);
+    }
+    */
+
+    /******************************
+     * Paso 2: Cada proceso calcula sus sumas parciales
+     ******************************/
+    double miSUMAx = 0.0, miSUMAy = 0.0, miSUMAxy = 0.0, miSUMAxx = 0.0;
+    for (int j = 0; j < mis_puntos; ++j) {
+        double xv = x_local[j];
+        double yv = y_local[j];
+        miSUMAx += xv;
+        miSUMAy += yv;
+        miSUMAxy += xv * yv;
+        miSUMAxx += xv * xv;
+    }
+
+    /* Liberar memoria local de los datos (opcional) */
+    free(x_local);
+    free(y_local);
+    if (mi_id == 0) {
+        free(x_full);
+        free(y_full);
+    }
+
+    /******************************
+     * Paso 3: Reducción de las sumas parciales usando un árbol binario
+     *           (pairwise / recursive halving style)
+     *
+     * Implementación:
+     *  - empaquetamos las 4 sumas en un vector double sums[4]
+     *  - en cada paso 'step' los procesos cuyos rangos son múltiplos de 2*step
+     *    esperan recibir desde rank+step (si existe), suman localmente y continúan.
+     *  - los procesos que no son múltiplos de 2*step envían sus datos a rank-step y salen.
+     *
+     * Usamos comunicaciones no bloqueantes (Isend/Irecv + Wait) para experimentar.
+     ******************************/
+    double sums[4];
+    sums[0] = miSUMAx;
+    sums[1] = miSUMAy;
+    sums[2] = miSUMAxy;
+    sums[3] = miSUMAxx;
+
+    int size = numero_procesos;
+    int step = 1;
+
+    while (step < size) {
+        if ((mi_id % (2 * step)) == 0) {
+            int src = mi_id + step;
+            if (src < size) {
+                double recvbuf[4];
+                MPI_Request rreq;
+                MPI_Irecv(recvbuf, 4, MPI_DOUBLE, src, 200 + step, MPI_COMM_WORLD, &rreq);
+                MPI_Wait(&rreq, &status);
+                /* acumular las sumas recibidas */
+                for (int k = 0; k < 4; ++k) sums[k] += recvbuf[k];
+            }
+            /* si src >= size no hay emisor en este paso para este receptor */
+        } else {
+            /* proceso emisor: calcula el destino y manda, luego sale del bucle */
+            int dst = mi_id - step;
+            MPI_Request sreq;
+            MPI_Isend(sums, 4, MPI_DOUBLE, dst, 200 + step, MPI_COMM_WORLD, &sreq);
+            MPI_Wait(&sreq, &status);
+            /* después de enviar, este proceso no participa en pasos superiores */
+            break;
+        }
+        step *= 2;
+    }
+
+    /* Al final, el proceso 0 tiene las sumas totales en sums[] */
+    if (mi_id == 0) {
+        double SUMAx = sums[0];
+        double SUMAy = sums[1];
+        double SUMAxy = sums[2];
+        double SUMAxx = sums[3];
+
+        /* calcular pendiente e interseccion y y otros resultados */
+        double pendiente = (SUMAx * SUMAy - n * SUMAxy) / (SUMAx * SUMAx - n * SUMAxx);
+        double interseccion_y = (SUMAy - pendiente * SUMAx) / (double) n;
+
+        printf("\nResultado (proceso 0):\n");
+        printf("  n = %d, procesos = %d\n\n", n, numero_procesos);
+        printf("  Pendiente (m) = %12.6f\n", pendiente);
+        printf("  Intersección y (b) = %12.6f\n\n", interseccion_y);
+
+        /* opcional: si queremos imprimir residuales, necesitamos los datos completos
+         * que antes tenía el proceso 0 en x_full,y_full. Como los liberamos para ahorrar memoria,
+         * podemos reabrir el archivo y volver a leer para mostrar los residuales.
+         * (Esto es opcional y puede quitarse si no se desea.)
+         */
+        FILE *f = fopen("datos_xy.txt", "r");
+        if (f) {
+            int nn;
+            fscanf(f, "%d", &nn);
+            double xi, yi;
+            printf("   Original (x,y)     Y estimado     Residual\n");
+            printf("--------------------------------------------------\n");
+            double suma_residual = 0.0;
+            for (int i = 0; i < nn; ++i) {
+                fscanf(f, "%lf %lf", &xi, &yi);
+                double y_est = pendiente * xi + interseccion_y;
+                double resid = yi - y_est;
+                suma_residual += resid * resid;
+                printf("   (%8.4lf, %8.4lf)    %12.6lf   %12.6lf\n", xi, yi, y_est, resid);
+            }
+            printf("--------------------------------------------------\n");
+            printf("Suma residual = %12.6f\n", suma_residual);
+            fclose(f);
+        } else {
+            printf("No se ha podido reabrir datos_xy.txt para mostrar residuales.\n");
+        }
+    }
+
+    MPI_Finalize();
+    return 0;
 }
